a7d911c9f4d226c1cd277e007dfd1c615341137f
JosephBean <kk1593@naver.com>
2025-01-07 18:09:23 +0900

    JPA 기본

--- src/main/java/com/java/JpaStudyApplication.java
+++ src/main/java/com/java/JpaStudyApplication.java
@@ -4,8 +4,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.java.domain.TestEntity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@RequiredArgsConstructor
 @Slf4j
 @SpringBootApplication
 public class JpaStudyApplication implements CommandLineRunner {
@@ -14,8 +21,37 @@ public class JpaStudyApplication implements CommandLineRunner {
 		SpringApplication.run(JpaStudyApplication.class, args);
 	}
 
+	private final EntityManagerFactory emf;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		
+		try {
+			et.begin();
+			// JPA를 처리하는 코드를 작성하는 영역
+			// 입력
+//			TestEntity t = new TestEntity();
+//			t.setTxt("8교시");
+//			em.persist(t);
+			
+			// 가져오기
+//			TestEntity t = em.find(TestEntity.class, 1);
+//			log.info("Find : {}", t);
+			
+			// 수정
+//			t.setTxt2("값 추가");
+			
+			// 삭제
+//			em.remove(t);
+			
+			et.commit();
+		} catch (Exception e) {
+			et.rollback();
+		} finally {
+			em.close();
+		}
+		
 	}
-
 }
